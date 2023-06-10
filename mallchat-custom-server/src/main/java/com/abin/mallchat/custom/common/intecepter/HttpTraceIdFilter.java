5ff407458bb07237458232ce70afb77cd0574e4d
xiemingquan <1932576789@qq.com>
2023-06-10 21:45:08 +0800

    本地测试配置启动链接

--- mallchat-custom-server/src/main/java/com/abin/mallchat/custom/common/intecepter/HttpTraceIdFilter.java
+++ mallchat-custom-server/src/main/java/com/abin/mallchat/custom/common/intecepter/HttpTraceIdFilter.java
@@ -1,11 +1,14 @@
 package com.abin.mallchat.custom.common.intecepter;
 
 import com.abin.mallchat.common.common.constant.MDCKey;
+import com.google.common.net.HttpHeaders;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -20,8 +23,18 @@ public class HttpTraceIdFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String tid = UUID.randomUUID().toString();
-        MDC.put(MDCKey.TID, tid);
+//        String tid = UUID.randomUUID().toString();
+//        MDC.put(MDCKey.TID, tid);
+//        chain.doFilter(request, response);
+
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        httpResponse.setHeader("Access-Control-Allow-Origin", httpRequest.getHeader(HttpHeaders.ORIGIN));
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpResponse.setHeader("Access-Control-Allow-Headers",
+                "*, Authorization, Origin, User-Agent, Referer, Accept, Content-type, languageType, Cache-Control, Pragma, Expires");
         chain.doFilter(request, response);
     }
 
